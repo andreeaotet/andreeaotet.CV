3673f63be3e95c67457b1dce7808da62286f3eb1
Cosmin <cosminotet@gmail.com>
2018-11-19 21:41:39 +0200

    add simplified function for show/hide

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ function $(id) {
     return document.getElementById(id);
 }
 
-
 function hide(id) {
     $(id).style.display = "none";
 }
@@ -21,69 +20,96 @@ function show(id) {
     $(id).style.display = "block";
 }
 
-function showSkillsPage() {
-    // console.log('show page');
-    hide("languages-page");
-    hide("home-page");
-    hide("experience-page");
-    hide("education-page");
-    hide("contact-page");
-    show("skills-page");
+// function showSkillsPage() {
+//     hide("languages-page");
+//     hide("home-page");
+//     hide("experience-page");
+//     hide("education-page");
+//     hide("contact-page");
+//     show("skills-page");
+// }
+
+// function showLanguagesPage() {
+//     hide("home-page");
+//     hide("skills-page");
+//     hide("experience-page");
+//     hide("education-page");
+//     hide("contact-page");
+//     show("languages-page");
+// }
+
+// function showHomePage() {
+//     hide("languages-page");
+//     hide("skills-page");
+//     hide("experience-page");
+//     hide("education-page");
+//     hide("contact-page");
+//     show("home-page");
+// }
+
+// function showExperiencePage() {
+//     hide("home-page");
+//     hide("skills-page");
+//     hide("languages-page");
+//     hide("education-page");
+//     hide("contact-page");
+//     show("experience-page");
+// }
+
+// function showEducationPage() {
+//     hide("home-page");
+//     hide("skills-page");
+//     hide("experience-page");
+//     hide("languages-page");
+//     hide("contact-page");
+//     show("education-page");
+// }
+
+// function showContactPage() {
+//     hide("home-page");
+//     hide("skills-page");
+//     hide("experience-page");
+//     hide("education-page");
+//     hide("languages-page");
+//     show("contact-page");
+// }
+
+// $("home-menu").onclick = showHomePage;
+// $("skills-menu").onclick = showSkillsPage;
+// $("education-menu").onclick = showEducationPage;
+// $("experience-menu").onclick = showExperiencePage;
+// $("contact-menu").onclick = showContactPage;
+// $("languages-menu").onclick = showLanguagesPage;
+
+function hideElement(page) {
+    page.style.display = "none";
 }
 
-document.getElementById("skills-menu").onclick = showSkillsPage;
+function hideAllPages() {
+    var pages = document.querySelectorAll(".page-block");
+    // pages.forEach(function(page) {
+    //     console.info("hide : " + page.id);
+    //     page.style.display = "none";
+    // });
+    pages.forEach(hideElement);
 
-function showLanguagesPage() {
-    hide("home-page");
-    hide("skills-page");
-    hide("experience-page");
-    hide("education-page");
-    hide("contact-page");
-    show("languages-page");
+    // for (var i = 0; i < pages.length; i++) {
+    //     pages[i].style.display = 'none';
+    // }
 }
 
-document.getElementById("languages-menu").onclick = showLanguagesPage;
 
-function showHomePage() {
-    hide("languages-page");
-    hide("skills-page");
-    hide("experience-page");
-    hide("education-page");
-    hide("contact-page");
-    show("home-page");
+var links = document.querySelectorAll('.topnav .menu-item');
+for (var i = 0; i < links.length; i++) {
+    // console.log(links[i].getAttribute("data-page"), links[i]);
+
+    links[i].onclick = function () {
+        // console.warn("hide all pages");
+        hideAllPages();
+        var page = this.getAttribute('data-page');
+        // console.info("show page", page);
+        show(page + "-page");
+    };
 }
 
-document.getElementById("home-menu").onclick = showHomePage;
-
-function showExperiencePage() {
-    hide("home-page");
-    hide("skills-page");
-    hide("languages-page");
-    hide("education-page");
-    hide("contact-page");
-    show("experience-page");
-}
-
-document.getElementById("experience-menu").onclick = showExperiencePage;
-
-function showEducationPage() {
-    hide("home-page");
-    hide("skills-page");
-    hide("experience-page");
-    hide("languages-page");
-    hide("contact-page");
-    show("education-page");
-}
-
-document.getElementById("education-menu").onclick = showEducationPage;
-
-function showContactPage() {
-    hide("home-page");
-    hide("skills-page");
-    hide("experience-page");
-    hide("education-page");
-    hide("languages-page");
-    show("contact-page");
-}
-
-document.getElementById("contact-menu").onclick = showContactPage;
+show('home-page');
